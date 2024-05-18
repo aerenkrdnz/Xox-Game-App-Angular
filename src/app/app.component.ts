bc92db190d64678af83a44ef49aab9a349983dc5
Ali Eren Karadeniz <aerenkrdnz@gmail.com>
2024-05-18 22:28:04 +0300

    Initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,47 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+
 })
 export class AppComponent {
-  title = 'xox-game-app';
+  mark: string = "";
+  message: string = "";
+  games: string[] = [];
+  moves: any = [];
+  
+  constructor(){
+    this.newGame();
+  }
+
+  newGame() {
+    this.mark = "X";
+    this.message = "Sıradaki: X";
+    this.games = [
+      "",
+      "",
+      "",
+      "",
+      "",
+      "",
+      "",
+      "",
+      "",
+
+    ];
+  }
+
+  setMark(index: number) {
+    if(this.games[index]==""){
+      this.games[index] = this.mark;
+      this.moves.push([...this.games]);
+      if (this.mark == "X") this.mark = "O"
+      else this.mark = "X"
+      this.message = `Sıradaki: ${this.mark}`
+    }
+    
+  }
+
+  returnSelectMove(index: number){
+    this.games = this.moves[index];
+  }
 }
